580376ac452e32b232da7fd4602f7cb7dccb072e
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-06-30 21:13:20 +0300

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+import api_key from "../data/api_key.js";
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/api_key.js
@@ -0,0 +1,1 @@
+export default "meQ9fvuDtgc0Jww8usDhcKC9f7evVbVk";
