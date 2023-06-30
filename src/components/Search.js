653824b1d98fdb905234457d883d0779f07e7ae0
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-07-01 01:05:35 +0300

    feat: create search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import "../pages/Search.css";
+
+import { api } from '../utils/api';
+
+import useFormAndValidation from '../hooks/useFormAndValidation';
+
+import resetIcon from "../images/reset.svg";
+import searchIcon from "../images/search.svg";
 
 const Search = () => {
+  const { values, handleChange, handleBlur, isValid, errors, resetForm } = useFormAndValidation();
+
+  const [gifs, setGifs] = useState([]);
+  const [lastSearchString, setLastSearchString] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  const handleInputChange = (evt) => {
+    handleChange(evt);
+  }
+
+  const handleReset = () => {
+    resetForm();
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    handleSearch();
+  }
+
+  const handleSearch = () => {
+    // Ограничение поиска по одинаковым запросам при сабмите формы
+    if (isValid && values.search && lastSearchString !== values.search) {
+      setLastSearchString(values.search);
+
+      api.searchGifs(values.search, 20, 0)
+        .then(data => {
+          setGifs(data);
+          console.log(data); // Вовод на экран число для теста
+        });
+    }
+  }
+
+  useEffect(() => {
+    // Создание задержки поиска при вводе поискового запроса
+    // Чтобы не генерировать много лишних запросов в момент ввода
+    if (searchTimeout) {
+      clearTimeout(searchTimeout);
+    }
+
+    setSearchTimeout(setTimeout(() => {
+      handleSearch();
+      setSearchTimeout(null);
+    }, 1000));
+
+  }, [values]);
+
   return (
-    <div>
-      
+    <div className='search'>
+      <form action="" className="search__form" onSubmit={handleSubmit}>
+        <div className="search__input-wrapper">
+          <input
+            type="text"
+            name='search'
+            required
+            minLength={2}
+            maxLength={40}
+            className={`search__input ${errors.search ? "search__input_invalid" : ""}`}
+            placeholder='Найди свою идеальную гифку'
+            value={values.search || ""}
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+          />
+          <p className={`search__input-error ${errors.search ? "" : "search__input-error_hidden"}`}>{errors.search}</p>
+        </div>
+
+        <button type="reset" className='search__button' onClick={handleReset}>
+          <img src={resetIcon} alt="Очистить поле запроса, кнопка" />
+        </button>
+
+        <button type="submit" className='search__button' disabled={!isValid}>
+          <img src={searchIcon} alt="Выполнить поиск, кнопка" />
+        </button>
+      </form>
     </div>
   );
 };
---
+++ src/utils/api.js
@@ -0,0 +1,27 @@
+import api_key from "../data/api_key.js";
+
+class Api {
+  constructor(base_url, api_key) {
+    this.base_url = base_url;
+    this.api_key = api_key;
+  }
+
+  async makeRequest(url, options) {
+    return fetch(url, options)
+      .then(this._checkResponse);
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка ${res.status}`);
+  }
+
+  searchGifs(string, limit = 9, offset = 0) {
+    return this.makeRequest(`${this.base_url}?api_key=${api_key}&q=${string}&limit=${limit}&offset=${offset}`);
+  }
+}
+
+
+export const api = new Api("https://api.giphy.com/v1/gifs/search", api_key);
