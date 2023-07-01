9997996e7ea7e5d166bf27b5cc6ea3b86d74ce99
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-07-01 16:10:58 +0300

    fix: import necessary components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from 'react-router-dom';
+
+import { api } from "../utils/api";
 
 import Header from './Header';
 import Search from './Search';
