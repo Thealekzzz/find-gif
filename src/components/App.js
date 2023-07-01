23ec1f88b72005cebc6a22ce1d37deec4e84e8d1
Daria Rutts <darya.rutts@yandex.ru>
2023-07-01 15:21:54 +0300

    feature: get random gif

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,36 @@ import { api } from "../data/api";
 
 function App() {
   const [gifs, setGifs] = useState([]);
+  const [randomGif, setRandomGif] = useState({});
 
   // get trends
   useEffect(() => {
     api
       .trendingGifs()
       .then((gif) => {
-        setGifs(gif.data.map((item) => ({
-          id: item.id,
-          alt: item.title,
-          src: item.images.original.url,
-        })));
+        setGifs(
+          gif.data.map((item) => ({
+            id: item.id,
+            alt: item.title,
+            src: item.images.original.url,
+          }))
+        );
       })
       .catch(console.error);
   }, []);
 
-  return <Main gifs={gifs} />;
+  // get random gif
+  useEffect(() => {
+    api
+      .randomGif()
+      .then((gif) => {
+        setRandomGif(gif.data);
+        console.log(gif.data);
+      })
+      .catch(console.error);
+  }, []);
+
+  return <Main gifs={gifs} randomGif={randomGif} />;
 }
 
 export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./Button";
 import Card from "./Card";
 
-const Main = ({ gifs }) => {
+const Main = ({ gifs, randomGif }) => {
   return (
     <main className="content">
       <section className="cards">
@@ -11,6 +11,11 @@ const Main = ({ gifs }) => {
         })}
       </section>
       <Button />
+
+      <section className="random">
+        <img className="random__image" src={randomGif.images.downsized_large.url} alt={randomGif.title} />
+        <Button/>
+      </section>
     </main>
   );
 };
