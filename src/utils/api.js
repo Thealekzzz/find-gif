5ad35c37c2e445ff781307e3ef2045615b2ecd7d
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-07-01 12:14:11 +0300

    fix: change baseUrl

--- src/utils/api.js
+++ src/utils/api.js
@@ -19,9 +19,9 @@ class Api {
   }
 
   searchGifs(string, limit = 9, offset = 0) {
-    return this.makeRequest(`${this.base_url}?api_key=${api_key}&q=${string}&limit=${limit}&offset=${offset}`);
+    return this.makeRequest(`${this.base_url}/search?api_key=${api_key}&q=${string}&limit=${limit}&offset=${offset}`);
   }
 }
 
 
-export const api = new Api("https://api.giphy.com/v1/gifs/search", api_key);
+export const api = new Api("https://api.giphy.com/v1/gifs", api_key);
