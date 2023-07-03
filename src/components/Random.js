3a92fa90d68785b913e8577df163f480f6d92ecb
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-07-03 13:51:49 +0300

    feat: button text & loading state

--- src/components/Random.js
+++ src/components/Random.js
@@ -3,14 +3,17 @@ import { api } from "../utils/api";
 import Button from './Button';
 
 const Random = () => {
-  const [randomGif, setRandomGif] = useState({});
-  const [buttonText, setButtonText] = useState('Давай еще гифку!');
   const textsList = [
     'Давай еще гифку!',
     'И еще одну!',
     'А можно еще? 🥺',
     'Следующая!'
-  ]
+  ];
+
+  const [randomGif, setRandomGif] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [gifNumber, setGifNumber] = useState(0);
+  const [buttonText, setButtonText] = useState(textsList[gifNumber]);
 
   useEffect(() => {
     api
@@ -19,31 +22,39 @@ const Random = () => {
         setRandomGif(gif.data);
       })
       .catch(console.error);
+
   }, []);
 
+  useEffect(() => {
+    setButtonText(textsList[Math.min(gifNumber, textsList.length - 1)]);
+  }, [gifNumber]);
+
   function handleClick() {
+    setIsLoading(true);
+    setRandomGif({});
     api
       .randomGif()
       .then((gif) => {
         setRandomGif(gif.data);
+        setGifNumber(prev => ++prev);
       })
       .catch(console.error)
       .finally(() => {
-        setButtonText(textsList[Math.floor(Math.random() * textsList.length)])
-      })      
+        setIsLoading(false);
+      });
   }
 
   return randomGif.url
     ? (
       <section className="random">
         <img className="random__image" src={randomGif?.images?.downsized_large.url} alt={randomGif.title} />
-        <Button onClick={handleClick}>{buttonText}</Button>
+        <Button onClick={handleClick} isLoading={isLoading}>{buttonText}</Button>
       </section>
     ) : (
       <div className="loading">
         <span className='spinner spinner_black spinner_size_L'></span>;
       </div>
-    )
+    );
 
 };
 
