a5edef08ccdb3c28a156e511bd3b928338c08297
Daria Rutts <darya.rutts@yandex.ru>
2023-07-04 12:56:29 +0300

    refactor: increase the limit to 12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
-
-import Header from './Header';
-import Search from './Search';
-import Trends from './Trends';
-import Random from './Random';
+import { Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Search from "./Search";
+import Trends from "./Trends";
+import Random from "./Random";
 
 function App() {
-  const limit = 9;
+  const limit = 12;
 
   return (
     <div className="page">
       <Header />
-
       <Routes>
         <Route path="/" element={<Search limit={limit} />} />
         <Route path="/trends" element={<Trends limit={limit} />} />
@@ -20,7 +18,6 @@ function App() {
       </Routes>
     </div>
   );
-
 }
 
 export default App;
