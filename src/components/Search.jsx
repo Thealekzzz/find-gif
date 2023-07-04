796d1f3bb09e5d88aca4c87415ce306d73429cca
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-07-04 21:28:37 +0300

    fix: get rid of duplicating gifs on search click

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -17,7 +17,7 @@ const Search = ({ limit }) => {
     handleBlur,
     isValid,
     errors,
-    resetForm
+    resetForm,
   } = useFormAndValidation();
 
   const textsList = [
@@ -49,22 +49,24 @@ const Search = ({ limit }) => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    handleSearch();
-  };
+    handleSearch(values.search, false);
+};
 
-  function handleNextButtonClick() {
-    setOffset((prev) => prev + limit);
-  }
+function handleNextButtonClick() {
+  setOffset((prev) => prev + limit);
+}
 
-  const handleSearch = (extend = true, searchValue = null) => {
+const handleSearch = (searchValue, extend = true) => {
+  // Ограничение поиска по одинаковым запросам при сабмите формы
+  // Если инпут валиден и (предыдущий запрос не равен текущему или нужно добавить еще гифок по тому же запросу)
+  if (isValid && (lastSearchString !== values.search || extend)) {
+    setLastSearchString(values.search);
     setIsLoading(true);
 
-    if (!extend) {
-      setGifs([]);
-    }
+    !extend && setGifs([]);
 
     api
-      .searchGifs(searchValue || values.search, limit, offset)
+      .searchGifs(searchValue, limit, offset)
       .then((recievedGifs) => {
         const newGifs = [
           ...(extend ? gifs : []),
@@ -82,114 +84,111 @@ const Search = ({ limit }) => {
       .finally(() => {
         setIsLoading(false);
       });
-  };
+  }
+};
 
-  // Поиск гифок при вводе запроса
-  useEffect(() => {
-    // Задание состояния загрузки
-    if (isValid) {
-      setIsLoading(true);
-    } else {
-      setIsLoading(false);
-    }
+// Поиск гифок при вводе запроса
+useEffect(() => {
+  // Задание состояния загрузки
+  if (isValid) {
+    setIsLoading(true);
+  } else {
+    setIsLoading(false);
+  }
 
-    // Отмена таймаута выполнения поиска при очередном нажатии кнопки
-    if (searchTimeout) {
-      clearTimeout(searchTimeout);
-    }
+  // Отмена таймаута выполнения поиска при очередном нажатии кнопки
+  if (searchTimeout) {
+    clearTimeout(searchTimeout);
+  }
 
-    // Ограничение поиска по одинаковым запросам при сабмите формы
-    if (isValid && lastSearchString !== values.search) {
-      setLastSearchString(values.search);
 
-      // Создание задержки поиска при вводе поискового запроса
-      // Чтобы не генерировать много лишних запросов в момент ввода
-      setSearchTimeout(
-        setTimeout(() => {
-          handleSearch(false);
-          setSearchTimeout(null);
-        }, 1000)
-      );
-    }
-    // eslint-disable-next-line
-  }, [values]);
+  // Создание задержки поиска при вводе поискового запроса
+  // Чтобы не генерировать много лишних запросов в момент ввода
+  setSearchTimeout(
+    setTimeout(() => {
+      handleSearch(values.search, false);
+      setSearchTimeout(null);
 
-  // Добавление гифок при нажатии кнопки
-  useEffect(() => {
-    if (offset !== 0) {
-      handleSearch(true);
-    }
-
-    // Изменение текста кнопки в зависимости от страницы
-    const currentPage = offset / limit;
-    setButtonText(textsList[Math.min(currentPage, textsList.length - 1)]);
-
-    // eslint-disable-next-line
-  }, [offset]);
-
-  return (
-    <section className="search">
-      <div className="search__container">
-        <form action="" className="search__form" onSubmit={handleSubmit}>
-          <div className="search__input-wrapper">
-            <input
-              type="text"
-              name="search"
-              required
-              autoComplete="off"
-              minLength={2}
-              maxLength={40}
-              className={`search__input ${
-                errors.search ? "search__input_invalid" : ""
-              }`}
-              placeholder="Найди свою идеальную гифку"
-              value={values.search || ""}
-              onChange={handleInputChange}
-              onBlur={handleBlur}
-            />
-            <p
-              className={`search__input-error ${
-                errors.search ? "" : "search__input-error_hidden"
-              }`}
-            >
-              {errors.search}
-            </p>
-          </div>
-          <SearchButton
-            type="reset"
-            disabled={false}
-            onButtonClick={handleReset}
-            src={resetIcon}
-            alt="Очистить поле запроса, кнопка"
-          />
-          <SearchButton
-            type="submit"
-            disabled={!isValid}
-            src={searchIcon}
-            alt="Выполнить поиск, кнопка"
-          />
-        </form>
-      </div>
-
-      {gifs.length ? (
-        <Main
-          gifs={gifs}
-          onNextButtonClick={handleNextButtonClick}
-          buttonText={buttonText}
-          isLoading={isLoading}
-          isMoreGifs={offset + limit < totalGifs}
-        />
-      ) : (
-        <>
-          {isLoading ? (
-            <Loader className="spinner_size_L" />
-          ) : (
-            <Infotip isValid={isValid} />
-          )}
-        </>
-      )}
-    </section>
+    }, 1000)
   );
+
+  // eslint-disable-next-line
+}, [values.search]);
+
+// Добавление гифок при нажатии кнопки
+useEffect(() => {
+  if (offset !== 0) {
+    handleSearch(values.search, true);
+  }
+
+  // Изменение текста кнопки в зависимости от страницы
+  const currentPage = offset / limit;
+  setButtonText(textsList[Math.min(currentPage, textsList.length - 1)]);
+
+  // eslint-disable-next-line
+}, [offset]);
+
+return (
+  <section className="search">
+    <div className="search__container">
+      <form action="" className="search__form" onSubmit={handleSubmit}>
+        <div className="search__input-wrapper">
+          <input
+            type="text"
+            name="search"
+            required
+            autoComplete="off"
+            minLength={2}
+            maxLength={40}
+            className={`search__input ${errors.search ? "search__input_invalid" : ""
+              }`}
+            placeholder="Найди свою идеальную гифку"
+            value={values.search || ""}
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+          />
+          <p
+            className={`search__input-error ${errors.search ? "" : "search__input-error_hidden"
+              }`}
+          >
+            {errors.search}
+          </p>
+        </div>
+        <SearchButton
+          type="reset"
+          disabled={false}
+          onButtonClick={handleReset}
+          src={resetIcon}
+          alt="Очистить поле запроса, кнопка"
+        />
+        <SearchButton
+          type="submit"
+          disabled={!isValid}
+          src={searchIcon}
+          alt="Выполнить поиск, кнопка"
+        />
+      </form>
+    </div>
+
+    {gifs.length ? (
+      <Main
+        gifs={gifs}
+        onNextButtonClick={handleNextButtonClick}
+        buttonText={buttonText}
+        isLoading={isLoading}
+        isMoreGifs={offset + limit < totalGifs}
+      />
+    ) : (
+      <>
+        {isLoading ? (
+          <Loader className="spinner_size_L" />
+        ) : (
+          <Infotip isValid={isValid} />
+        )}
+      </>
+    )}
+  </section>
+);
 };
 
 export default Search;
